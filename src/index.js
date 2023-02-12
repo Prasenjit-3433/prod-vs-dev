4d7c836235c2c0cad477f13dff836f88618ad8fe
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-02-12 11:30:40 +0530

    project set up

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
+
+const heading = new Heading();
+heading.render();
+
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
+
+let ten = 10;
